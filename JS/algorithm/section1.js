ad830139593e8c26f2f23d3276abc6c2d494d899
yh9035926 <109195043+yh9035926@users.noreply.github.com>
2023-10-17 11:57:43 +0900

    ...arr

--- JS/algorithm/section1.js
+++ JS/algorithm/section1.js
@@ -21,3 +21,34 @@ console.log(solution(6, 7, 11));  //6
 }
 console.log(solution(110, 23, 35)); //NO */
 
+//연필 개수
+/* function solution(n) {
+  let answer = Math.ceil(a / 12);
+  return answer;
+}
+console.log(solution(178));
+ */
+
+//1부터 N까지 합 출력
+/* function solution(n) {
+  let answer = 0;
+  for (let i = 1; i <= n; i++) {
+    answer = answer + i;
+  }
+  return answer;
+}
+console.log(solution(10));
+ */
+
+//최솟값 구하기
+/* function solution() {
+  let arr = [];
+  for (let i = 0; i < 7; i++) {
+    let number = Math.floor(Math.random() * 100);
+    arr.push(number);
+  }
+  let answer = Math.min(...arr); //스프레드는 배열 요소 직접 연결
+  console.log(arr, answer);
+  return answer;
+}
+solution(); */
